178cd80f61354a38e9eaa97e608e45e5bb2833f2
Josh De Winne <josh@dewinne.dev>
2023-10-16 15:24:29 -0700

    Adding example that uses the kubeconfig

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -1,19 +1,13 @@
-terraform {
-  required_providers {
-    replicated = {
-      source = "registry.terraform.io/replicated/replicated"
-    }
-  }
-}
-provider "replicated" {
+module "kind" {
+  source = "./kind"
 }
 
-resource "replicated_cluster" "tf_cluster" {
-  name          = "terraformCLuster"
-  distribution  = "kind"
-  wait_duration = "20m"
-  ttl           = "10m"
-  instance_type = "r1.large"
-  nodes         = 1
-  disk          = 100
+module "kubeconfig" {
+    source = "./kubeconfig"
+    kubeconfig = module.kind.kind_kubeconfig
+}
+
+module "app" {
+    source = "./app"
+    kubeconfig_location = module.kubeconfig.kubeconfig_location
 }
